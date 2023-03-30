3b32104f566fce7e0f2e3bb9cd81518541ebc871
Md-Ibnul <ibnulhasan456@gmail.com>
2023-03-30 16:29:34 +0600

    calculation added

--- src/components/shop/Shop.jsx
+++ src/components/shop/Shop.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { addToDb, getShoppingCart } from '../../utilities/fakedb.js';
 import Cart from '../cart/Cart.jsx';
 import Product from '../product/Product.jsx';
 import './Shop.css';
@@ -14,9 +15,31 @@ const Shop = () => {
         .then(data => setProducts(data))
     }, []);
 
+    useEffect(() => {
+        const storedCart = getShoppingCart();
+        const saveCart = [];
+        // step-1 get id of the addedProduct
+        for(const id in storedCart){
+            // step-2 get product from product state by using id
+            const addedProduct = products.find(product => product.id === id);
+            if(addedProduct){
+            // step-3 added quantity
+            const quantity = storedCart[id];
+            addedProduct.quantity =quantity;
+            // step-4 add the addedProduct to the saved Cart
+            saveCart.push(addedProduct)
+        }
+        // step-5 set cart
+        setCart(saveCart);
+        }
+    },[products])
+
+
     const handleAddToCart = (product) => {
         const newCart = [...cart, product];
         setCart (newCart); 
+
+        addToDb(product.id)
     }
 
     return (
